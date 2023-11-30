63c7400dc7b691912b5de780ba2eb24cb3ccb56a
Jonas Bremer <118913929+scieneer-jonas@users.noreply.github.com>
2023-11-30 16:08:46 +0100

    Project structure (#2)
    moochup loader
    embedding structure
    vertex ai mistral endpoint test
    openai inference

---
+++ src/extraction.py
@@ -0,0 +1,29 @@
+from src.loaders.moochup import get_course_payloads
+from src.loaders.payload import Payload
+from src.embedder.multilingual_e5_large import MultilingualE5LargeEmbedder
+from src.vectordb.qdrant import VectorDBQdrant
+
+# use all loaders 
+documents:list[Payload] = []
+course_payloads = get_course_payloads()
+documents.extend(course_payloads)
+
+# create embeddings
+embedder = MultilingualE5LargeEmbedder()
+embeddings = [embedder.embed(payload.vector_content, type='passage') for payload in documents]
+
+# store in db
+COLLECTION_NAME = 'test_collection'
+points = [{'id': i, 'vector': embedding, 'payload': payload.model_dump()} 
+          for i, (embedding, payload) in enumerate(zip(embeddings, documents))]
+print(f'Example point: {points[0]}')
+
+vectordb = VectorDBQdrant(collection_name=COLLECTION_NAME, vector_size=len(embeddings[0]))
+vectordb.upsert(collection_name=COLLECTION_NAME, points=points)
+
+# test
+while True:
+    query = input('Enter query: ')
+    query_embedding = embedder.embed(query)
+    search_result = vectordb.search(collection_name=COLLECTION_NAME, query_vector=query_embedding, with_payload=True)
+    print(search_result)
---
+++ src/vectordb/qdrant.py
@@ -0,0 +1,49 @@
+
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
+from qdrant_client.http.models import PointStruct
+from qdrant_client.http.models import Filter, FieldCondition, MatchValue
+from qdrant_client.http.exceptions import ResponseHandlingException, UnexpectedResponse
+
+class VectorDBQdrant():
+    def __init__(self, version='disk'):
+        self.version = version
+        if version == 'memory':
+            self.client = QdrantClient(':memory:')
+        elif version == 'disk':
+            self.client = QdrantClient('localhost', port=6333)
+        else:
+            raise ValueError("Version must be either 'memory' or 'disk'")
+        
+    def get_or_create_collection(self, collection_name, vector_size=None):
+        try:
+            _ = self.client.get_collection(collection_name=self.collection_name)
+        except ResponseHandlingException as e:
+            if self.version == 'disk':
+                print('Qdrant container not running? Run:')
+                print('docker run -p 6333:6333 -p 6334:6334 -v $(pwd)/qdrant_storage:/qdrant/storage:z qdrant/qdrant:v1.6.1')
+            raise e
+        except UnexpectedResponse as e:
+            self.client.create_collection(
+                collection_name=collection_name,
+                vectors_config=VectorParams(size=vector_size, distance=Distance.DOT),
+            )
+
+    def upsert(self, collection_name, points: list[dict]):
+        qdrant_points = [PointStruct(**point) for point in points]
+        operation_info = self.client.upsert(
+            collection_name=collection_name,
+            wait=True,
+            points=qdrant_points,
+        )
+        print(operation_info)
+
+    def search(self, collection_name, query_vector, query_filter=None, with_payload=True, limit=3):
+        search_result = self.client.search(
+            collection_name=collection_name,
+            query_vector=query_vector,
+            query_filter=query_filter,
+            with_payload=with_payload,
+            limit=limit,
+        )
+        return search_result
