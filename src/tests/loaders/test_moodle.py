f0ce09f00a4a5b800136ebb0858b4656cda2e702
Jonas Bremer <jonas.bremer@scieneers.de>
2024-02-05 16:51:46 +0100

    update test course

--- src/tests/loaders/test_moodle.py
+++ src/tests/loaders/test_moodle.py
@@ -4,13 +4,16 @@ import pytest
 import json
 
 def test_get_topics(course_id:int=16):
-    #course_contents = Moodle("STAGING").get_course_contents(course_id)
+    # https://ki-campus-test.fernuni-hagen.de/course/view.php?id=16
+    # course_contents = Moodle().get_course_contents(course_id)
     with open(f'src/tests/data/core_course_get_contents.json', 'r') as f:
         course_contents = json.load(f)
 
-    course_contents = [CourseTopic(**topic) for topic in course_contents]
+    courses = []
+    for course_content in course_contents:
+        course = CourseTopic(**course_content)
+        courses.append(course)
     
-    course_contents_dicts = [course_content.model_dump() for course_content in course_contents]
-
-    with open(f'src/tests/data/proccessed_course_contents.json', 'w') as f:
+    course_contents_dicts = [course.model_dump() for course in courses]
+    with open(f'outputs/proccessed_course_contents.json', 'w') as f:
         json.dump(course_contents_dicts, f, indent=4, ensure_ascii=False)
